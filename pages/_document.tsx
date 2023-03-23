1d7e84bbd109babaa462603bae2e089e96108ad6
Leafmire <ekwkqdk007@naver.com>
2023-03-23 20:28:32 +0900

    [ADD] index page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
-      <body>
+      <body className="w-full h-full bg-zinc-800">
         <Main />
         <NextScript />
       </body>
